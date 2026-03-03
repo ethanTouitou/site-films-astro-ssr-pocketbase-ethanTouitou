85d4d8edf53112256fe95816013424f44d585327
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2026-03-03 13:24:21 +0100

    test

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,28 +2,24 @@
 import PocketBase from 'pocketbase';
 import { defineMiddleware } from 'astro/middleware';
 
-export const onRequest = defineMiddleware(async ({ locals, request, isPrerendered }, next) => {
-    const pbUrl =
-        import.meta.env.PB_URL ||
-        (import.meta.env.MODE === 'development'
-            ? 'http://localhost:8090'
-            : 'https://site-films.ethantouitou.fr:443');
+export const onRequest = defineMiddleware(async ({ locals, request, isPrerendered }, next: () => any) => {
+    locals.pb = new PocketBase(import.meta.env.PB_URL);
 
-    locals.pb = new PocketBase(pbUrl);
-
-    if (!isPrerendered) {
+    if (!isPrerendered) {// load the store data from the request cookie string
         locals.pb.authStore.loadFromCookie(request.headers.get('cookie') || '');
 
         try {
+            // get an up-to-date auth store state by verifying and refreshing the loaded auth record (if any)
             locals.pb.authStore.isValid && await locals.pb.collection('users').authRefresh();
         } catch (_) {
+            // clear the auth store on failed refresh
             locals.pb.authStore.clear();
         }
     }
 
     const response = await next();
 
-    if (!isPrerendered) {
+    if (!isPrerendered) {// send back the default 'pb_auth' cookie to the client with the latest store state
         response.headers.append('set-cookie', locals.pb.authStore.exportToCookie());
     }
 
